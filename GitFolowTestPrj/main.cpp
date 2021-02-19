08fe30db1eeefc412c506440fe5cbca02230752b
vlad <nfk.if@i.ua>
2021-02-19 20:47:05 +0200

    make cool loop

--- GitFolowTestPrj/main.cpp
+++ GitFolowTestPrj/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <algorithm>
 
 int main(int agrc, char* argv[])
 {
-	for (size_t i = 0; i < agrc; i++)
-	{
-		std::cout << argv[i] << std::endl;
-	}
+	std::for_each(argv, (argv + agrc), [](const char* argument)
+		{
+			std::cout << argument << std::endl;
+		});
+
 	return 0;
 }
